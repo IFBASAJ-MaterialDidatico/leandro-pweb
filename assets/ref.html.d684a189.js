a0225f83a396f0020006db77d5705ad16fcd5739
leandro-costa <leandro.costa@ifba.edu.br>
2022-06-16 00:16:57 +0000

    Deploying to gh-pages from @ IFBASAJ-MaterialDidatico/leandro-pweb@b935a6a55665b3f9a3ba993d5d55082af5bc60a0 🚀

---
+++ assets/ref.html.d684a189.js
@@ -0,0 +1,1 @@
+import{_ as r,o as a,c,a as e,b as t}from"./app.9b60929d.js";const s={},n=e("h2",{id:"referencias",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#referencias","aria-hidden":"true"},"#"),t(" Refer\xEAncias")],-1),o=[n];function _(i,d){return a(),c("div",null,o)}var h=r(s,[["render",_],["__file","ref.html.vue"]]);export{h as default};
